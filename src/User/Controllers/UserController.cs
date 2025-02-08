ed8a57644f21556d2fc95a18c897d1fe34489fee
zeeshan khan <zeeshankhn123@outlook.com>
2025-02-08 12:27:48 +0000

    user controller missed

--- src/User/Controllers/UserController.cs
+++ src/User/Controllers/UserController.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApplication1.User.DTOs;
+using WebApplication1.User.Models;
 using WebApplication1.User.Services;
 using WebApplication1.Utils.Middleware;
 
 namespace WebApplication1.User.Controllers;
+
+public interface IUserController
+{
+    ActionResult<GetUserDto> GetUser();
+}
 
 [ApiController]
 [Route("api/user")]
@@ -26,6 +32,30 @@ public class UserController(IUserService userService) : ControllerBase, IUserCon
         {
             Console.WriteLine(e.Message);
             return StatusCode(401, "Unauthorized");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return StatusCode(500, "An error occurred while processing your request.");
+        }
+    }
+    
+    [Route("get-users-by-company")]
+    [HttpGet]
+    public ActionResult<List<UserRoleInfoModel>> GetUsersByCompany()
+    {
+        try
+        {
+
+            var companyId = HttpContext.Items["companyId"] as int? ?? throw new UnauthorizedAccessException("Company ID not found");
+            
+            var users = userService.GetUsersByCompany(companyId);
+
+            return Ok(users);
+        }
+        catch (UnauthorizedAccessException)
+        {
+            return Unauthorized("Unauthorized");
         }
         catch (Exception e)
         {
